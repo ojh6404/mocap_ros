fix rotation

node_scripts/hand_3d_node.py
@@ -12,6 +12,7 @@ from hand_object_detection_ros.msg import HandDetectionArray
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
+
 class Hand3DNode(object):
     def __init__(self):
         self.bridge = CvBridge()
@@ -19,15 +20,16 @@ class Hand3DNode(object):
         self.tf_broadcaster = tf.TransformBroadcaster()
 
         # Subscribe to the camera info and depth image topics
-        self.info_sub = mf.Subscriber('~camera_info', CameraInfo, buff_size=2**24)
-        self.depth_sub = mf.Subscriber('~input_depth', Image, buff_size=2**24)
-        self.hand_sub = mf.Subscriber('~input_detections', HandDetectionArray, buff_size=2**24)
-        self.ts = mf.ApproximateTimeSynchronizer([self.info_sub, self.depth_sub, self.hand_sub], queue_size=1, slop=0.15)
+        self.info_sub = mf.Subscriber("~camera_info", CameraInfo, buff_size=2**24)
+        self.depth_sub = mf.Subscriber("~input_depth", Image, buff_size=2**24)
+        self.hand_sub = mf.Subscriber("~input_detections", HandDetectionArray, buff_size=2**24)
+        self.ts = mf.ApproximateTimeSynchronizer(
+            [self.info_sub, self.depth_sub, self.hand_sub], queue_size=1, slop=0.15
+        )
         self.ts.registerCallback(self.callback)
 
         # Publisher for Pose
         self.pose_array_pub = rospy.Publisher("~hand_pose", PoseArray, queue_size=10)
-
 
     def callback(self, cam_info_data, depth_data, hand_data):
         # Extract the camera frame from the image message
@@ -45,7 +47,10 @@ class Hand3DNode(object):
             # Get the depth value at the 2D point
             point_2d = (detection.pose.position.x, detection.pose.position.y)
             # clip the point to the image size
-            point_2d = (min(max(detection.pose.position.x, 0), cv_image.shape[1] - 1), min(max(detection.pose.position.y, 0), cv_image.shape[0] - 1))
+            point_2d = (
+                min(max(detection.pose.position.x, 0), cv_image.shape[1] - 1),
+                min(max(detection.pose.position.y, 0), cv_image.shape[0] - 1),
+            )
             depth = cv_image[int(point_2d[1]), int(point_2d[0])]
             if np.isnan(depth):
                 continue
@@ -61,10 +66,7 @@ class Hand3DNode(object):
                 pose_msg.position.x = x_cam * 0.001
                 pose_msg.position.y = y_cam * 0.001
                 pose_msg.position.z = z_cam * 0.001
-                pose_msg.orientation.x = detection.pose.orientation.x
-                pose_msg.orientation.y = detection.pose.orientation.y
-                pose_msg.orientation.z = detection.pose.orientation.z
-                pose_msg.orientation.w = detection.pose.orientation.w
+                pose_msg.orientation = detection.pose.orientation
 
                 pose_array_msg = PoseArray()
                 pose_array_msg.header.frame_id = camera_frame
@@ -74,16 +76,19 @@ class Hand3DNode(object):
                 self.pose_array_pub.publish(pose_array_msg)
 
                 # Broadcast the transform
-                self.tf_broadcaster.sendTransform((pose_msg.position.x, pose_msg.position.y, pose_msg.position.z),
-                                                (detection.pose.orientation.x, detection.pose.orientation.y, detection.pose.orientation.z, detection.pose.orientation.w),
-                                                rospy.Time.now(),
-                                                detection.hand,
-                                                camera_frame)
+                self.tf_broadcaster.sendTransform(
+                    (pose_msg.position.x, pose_msg.position.y, pose_msg.position.z),
+                    (pose_msg.orientation.x, pose_msg.orientation.y, pose_msg.orientation.z, pose_msg.orientation.w),
+                    rospy.Time.now(),
+                    detection.hand,
+                    camera_frame,
+                )
 
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
                 rospy.logerr(e)
 
-if __name__ == '__main__':
-    rospy.init_node('hand_3d_node')
+
+if __name__ == "__main__":
+    rospy.init_node("hand_3d_node")
     node = Hand3DNode()
     rospy.spin()

node_scripts/hand_detection_node.py
@@ -7,6 +7,7 @@ import rospy
 import torch
 import numpy as np
 import cv2
+from scipy.spatial.transform import Rotation as R
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Pose
@@ -42,24 +43,24 @@ PASCAL_CLASSES = np.asarray(["__background__", "targetobject", "hand"])
 PALM_JOINTS = [0, 2, 5, 9, 13, 17]
 WEIGHTS = np.array([0.5, 0.1, 0.1, 0.1, 0.1, 0.1])
 
-def angle_axis_to_quaternion(angle_axis):
-    # Calculate the rotation angle (magnitude of the angle-axis vector)
-    theta = np.linalg.norm(angle_axis)
 
-    # Normalize the rotation axis
-    if theta > 0:
-        axis = angle_axis / theta
-    else:
-        # To handle the case of zero rotation gracefully
-        axis = np.zeros(3)
-
-    # Calculate the quaternion components
-    w = np.cos(theta / 2.0)
-    x = axis[0] * np.sin(theta / 2.0)
-    y = axis[1] * np.sin(theta / 2.0)
-    z = axis[2] * np.sin(theta / 2.0)
-
-    return np.array([w, x, y, z])
+def rotation_matrix_to_quaternion(R):
+    """Convert a rotation matrix to a quaternion.
+    Args:
+        R (np.ndarray): A 3x3 rotation matrix.
+    Returns:
+        np.ndarray: A 4-element unit quaternion.
+    """
+    q = np.empty((4,), dtype=np.float32)
+    M = R
+    q[0] = np.sqrt(np.maximum(0, 1 + M[0, 0] + M[1, 1] + M[2, 2])) / 2
+    q[1] = np.sqrt(np.maximum(0, 1 + M[0, 0] - M[1, 1] - M[2, 2])) / 2
+    q[2] = np.sqrt(np.maximum(0, 1 - M[0, 0] + M[1, 1] - M[2, 2])) / 2
+    q[3] = np.sqrt(np.maximum(0, 1 - M[0, 0] - M[1, 1] + M[2, 2])) / 2
+    q[1] *= np.sign(q[1] * (M[2, 1] - M[1, 2]))
+    q[2] *= np.sign(q[2] * (M[0, 2] - M[2, 0]))
+    q[3] *= np.sign(q[3] * (M[1, 0] - M[0, 1]))
+    return q
 
 
 def draw_axis(img, origin, axis, color, scale=20):
@@ -123,29 +124,35 @@ class HandObjectDetectionNode(object):
                         hand_orientation = pred_output_list[0][hand]["pred_hand_pose"][0, :3].astype(
                             np.float32
                         )  # angle-axis representation
-                        quat = angle_axis_to_quaternion(hand_orientation)
 
                         hand_pose = Pose()
                         hand_pose.position.x = hand_origin[0]
                         hand_pose.position.y = hand_origin[1]
-                        hand_pose.position.z = 0 # cause it's working in 2D
-                        hand_pose.orientation.x = quat[1]
-                        hand_pose.orientation.y = quat[2]
-                        hand_pose.orientation.z = quat[3]
-                        hand_pose.orientation.w = quat[0]
+                        hand_pose.position.z = 0  # cause it's working in 2D
 
                         for detection in detection_results.detections:
                             if detection.hand == hand:
                                 detection.pose = hand_pose
 
-                        R, _ = cv2.Rodrigues(hand_orientation)
-                        x_axis = np.array([1, 0, 0])
-                        y_axis = np.array([0, 1, 0])
-                        z_axis = np.array([0, 0, 1])
-
-                        x_axis_rotated = R @ x_axis
-                        y_axis_rotated = R @ y_axis
-                        z_axis_rotated = R @ z_axis
+                        rotation, _ = cv2.Rodrigues(hand_orientation)
+                        quat = rotation_matrix_to_quaternion(rotation)  # [w, x, y, z]
+                        if hand == "right_hand":
+                            x_axis = np.array([0, 0, 1])
+                            y_axis = np.array([0, -1, 0])
+                            z_axis = np.array([-1, 0, 0])
+                            rotated_result = R.from_rotvec(np.pi * np.array([1, 0, 0])) * R.from_quat(quat)
+                            quat = rotated_result.as_quat()  # [w, x, y, z]
+                        else:
+                            x_axis = np.array([0, 0, 1])
+                            y_axis = np.array([0, 1, 0])
+                            z_axis = np.array([1, 0, 0])
+                        hand_pose.orientation.x = quat[1]
+                        hand_pose.orientation.y = quat[2]
+                        hand_pose.orientation.z = quat[3]
+                        hand_pose.orientation.w = quat[0]
+                        x_axis_rotated = rotation @ x_axis
+                        y_axis_rotated = rotation @ y_axis
+                        z_axis_rotated = rotation @ z_axis
 
                         # visualize hand orientation
                         vis_im = draw_axis(vis_im, hand_origin, x_axis_rotated, (0, 0, 255))  # x: red
@@ -170,13 +177,7 @@ class HandObjectDetectionNode(object):
             hand_detection.state = (
                 "N"
                 if hand_det[5] == 0
-                else "S"
-                if hand_det[5] == 1
-                else "O"
-                if hand_det[5] == 2
-                else "P"
-                if hand_det[5] == 3
-                else "F"
+                else "S" if hand_det[5] == 1 else "O" if hand_det[5] == 2 else "P" if hand_det[5] == 3 else "F"
             )
             if hand_detection.state != "N" and obj_dets is not None:
                 hand_detection.object_rect = self.get_rect(obj_dets[img_obj_id[i]])