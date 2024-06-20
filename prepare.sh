22a06ebae12ee301fb55fceb10fca2208ee7a635
Jihoon Oh <oh@jsk.imi.i.u-tokyo.ac.jp>
2024-06-20 16:25:09 +0900

    fix setuptools version and install pyvirtualdisplay

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 git submodule update --init --recursive
-python3 -m pip install -U setuptools pip gdown
+python3 -m pip install -U pip gdown pyvirtualdisplay
+python3 -m pip install "setuptools<70"
 python3 -m pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 curl -LO https://github.com/NVIDIA/cub/archive/1.10.0.tar.gz
 tar xzf 1.10.0.tar.gz
